ab0cb3c6a229a0c584a7ed6b89001302d8a7349a
arturferreto <arturpessuto@gmail.com>
2023-08-16 17:32:21 -0300

    style: removendo Factory não utilizado

--- app/Models/UserSetting.php
+++ app/Models/UserSetting.php
@@ -2,14 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserSetting extends Model
 {
-    use HasFactory;
-
     /**
      * The connection name for the model.
      *
