1aeef55129ba50f09e966e29f92f16cf8aae7c24
arturferreto <arturpessuto@gmail.com>
2023-08-24 17:40:02 -0300

    feat: add input de username no perfil do usuário
    Signed-off-by: arturferreto <arturpessuto@gmail.com>

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -4,10 +4,21 @@ namespace App\Http\Requests;
 
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
 
 class ProfileUpdateRequest extends FormRequest
 {
+    /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'username' => Str::lower($this->username),
+        ]);
+    }
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -17,6 +28,7 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['string', 'max:255'],
+            'username' => ['string', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
