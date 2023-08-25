5d7a89f300213ee5009af2b06e8780ade829112e
arturferreto <arturpessuto@gmail.com>
2023-08-25 11:52:35 -0300

    feat: implementando o cadastro de um tenant já configurado para testes
    Signed-off-by: arturferreto <arturpessuto@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,11 @@
 
 namespace Tests;
 
+use App\Models\Company;
+use App\Models\Tenant;
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -18,16 +21,40 @@ abstract class TestCase extends BaseTestCase
                 'driver' => 'sqlite',
                 'database' => ':memory:',
             ],
-
-            'database.connections.tenant' => [
-                'driver' => 'sqlite',
-                'database' => ':memory:',
-            ]
         ]);
 
         $this->artisan('landlord:migrate --fresh');
-        $this->artisan('tenant:migrate --fresh');
 
         $this->app[Kernel::class]->setArtisan(null);
+
+        $this->setAndConfigureTenant();
+    }
+
+    private function setAndConfigureTenant(): void
+    {
+        $tenant = Tenant::firstOrCreate([
+            'name' => 'Tenant',
+            'slug' => 'tenant',
+            'database' => 'database/tenant.sqlite',
+            'total_users' => 100,
+        ]);
+
+        config([
+            'database.connections.tenant' => [
+                'driver' => 'sqlite',
+                'database' => $tenant->database,
+            ],
+            'cache.prefix' => $tenant->database,
+        ]);
+
+        DB::purge('tenant');
+
+        app('cache')->forgetDriver(config('cache.default'));
+
+        app()->forgetInstance('tenant');
+
+        app()->instance('tenant', $tenant);
+
+        $this->artisan("tenant:migrate $tenant->id --fresh");
     }
 }
